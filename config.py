48ee23bd3ba2361fe36f6d966f87861b09f49c39
wintze-chan <chanwt-pm24@student.tarc.edu.my>
2025-12-06 15:32:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.c72qtjffaoih.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+customdb = "employee_db"
+custombucket = "winz-bucket"
 customregion = "us-east-1"
 
